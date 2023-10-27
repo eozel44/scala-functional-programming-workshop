e5b3fc47fa5c57fe2e874ee6fba0445ffba5bee4
eren <eozel44@gmail.com>
2023-10-27 12:36:39 +0300

    functional data structure exercises

--- src/main/scala/s3_functional_datastructure.scala
+++ src/main/scala/s3_functional_datastructure.scala
@@ -6,7 +6,7 @@ object s3_functional_datastructure{
      which signals that A is a covariant or 'positive' parameter of List. This means that, for instance,
      List[Dog] is considered a subtype of List[Animal], assuming Dog is a subtype of Animal.
 
-     But notice now that Nil extends List[Nothing]. Nothing is a subtype of all types.
+     But notice now that Nil extenl List[Nothing]. Nothing is a subtype of all types.
      Nil can be considered a List[Int], a List[Double], and so on
    */
 
@@ -21,7 +21,7 @@ object s3_functional_datastructure{
         case Cons(head,tail) => head + sum(tail)
       }
     }
-    def product(ds: List[Double]):Double = ds match{
+    def product(l: List[Double]):Double = l match{
       case Nil => 1.0
       case Cons(0.0,_) => 0.0
       case Cons(head, tail) => head * product(tail)
@@ -32,12 +32,50 @@ object s3_functional_datastructure{
       else Cons(as.head, apply(as.tail: _*))
     }
 
+    /** exercises */
+    def remove[A](l:List[A], item: A):List[A]= l match{
+      case Nil => Nil
+      case Cons(head,tail) => if(head==item) tail else l
+    }
+
+    def setHead[A](item: A, l:List[A]):List[A]= l match{
+      case Nil => Cons(item,Nil)
+      case Cons(head,tail) =>  Cons(item,tail)
+    }
+
+    def drop[A](l: List[A], n: Int): List[A] = if(n<=0) l else l match{
+      case Nil => Nil
+      case Cons(head, tail) => drop(tail,n-1)
+    }
+
+    def dropWhile[A](l: List[A], f: A => Boolean): List[A] = l match {
+      case Cons(head, tail) => if (f(head)) tail else dropWhile(tail, f)
+      case _ => l
+    }
+
+    def append[A](l1:List[A], l2:List[A]):List[A] = l1 match {
+      case Nil => l2
+      case Cons(head, tail) => Cons(head,append(tail,l2))
+    }
+
+    def init[A](l: List[A]): List[A]  = l match{
+      case Nil => Nil
+      case Cons(_,Nil) => Nil
+      case Cons(head,tail) => Cons(head, init(tail))
+    }
   }
 
   def main(args: Array[String]): Unit = {
     val ex1: List[Double] = Nil
     val ex2: List[Int] = Cons(1, Nil)
     val ex3: List[String] = Cons("a", Cons("b",Nil))
+
+    val ex4 = List(1,2,3,4)
+
+    assert(List.remove(ex4,1) == List(2,3,4))
+    assert(List.drop(ex4, 2) == List(3,4))
+    assert(List.dropWhile(ex4,(x:Int) => x % 3 == 0) == List(4))
+    assert(List.init(ex4) == List(1,2,3))
 
 
 
