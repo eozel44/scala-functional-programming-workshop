5ae0b6ae5780b55ba0eb61105f01df3aeb6c0e5e
eren <eozel44@gmail.com>
2023-11-16 21:57:10 +0300

    code organization

--- src/main/scala/s5_laziness.scala
+++ src/main/scala/s5_laziness.scala
@@ -2,6 +2,44 @@ package laziness
 
 import Stream._
 
+
+object s5_laziness{
+
+  def main(args: Array[String]): Unit = {
+
+    def maybeTwice(b: Boolean, i: => Int) = if (b) i + i else 0
+
+    val x = maybeTwice(true, {
+      println("hi"); 1 + 41
+    })
+    //result
+    //hi
+    //hi
+    //x: Int = 84
+
+    /**
+     *  Adding the lazy keyword to a val declaration will cause Scala to delay
+     *  evaluation of the right hand side of that lazy val declaration until it is first referenced.
+     *  It will also cache the result so that subsequent references to it don't trigger repeated evaluation.
+     */
+    def maybeTwiceLazy(b: Boolean, i: => Int) = {
+      lazy val j = i
+      if (b) j + j else 0
+    }
+
+    val xx = maybeTwiceLazy(true, {
+      println("hi"); 1 + 41
+    })
+    //result
+    //hi
+    //x: Int = 84
+
+    assert(Stream(1, 2, 3).toList == List(1, 2, 3))
+    assert(Stream(1, 2, 3).take(2).toList == List(1, 2))
+
+  }
+}
+
 sealed trait Stream[+A]{
 
   def toList: List[A] = {
@@ -36,39 +74,4 @@ object Stream{
 
 }
 
-object s5_laziness{
 
-  def main(args: Array[String]): Unit = {
-
-    def maybeTwice(b: Boolean, i: => Int) = if (b) i + i else 0
-
-    val x = maybeTwice(true, {
-      println("hi"); 1 + 41
-    })
-    //result
-    //hi
-    //hi
-    //x: Int = 84
-
-    /**
-     *  Adding the lazy keyword to a val declaration will cause Scala to delay
-     *  evaluation of the right hand side of that lazy val declaration until it is first referenced.
-     *  It will also cache the result so that subsequent references to it don't trigger repeated evaluation.
-     */
-    def maybeTwiceLazy(b: Boolean, i: => Int) = {
-      lazy val j = i
-      if (b) j + j else 0
-    }
-
-    val xx = maybeTwiceLazy(true, {
-      println("hi"); 1 + 41
-    })
-    //result
-    //hi
-    //x: Int = 84
-
-    assert(Stream(1, 2, 3).toList == List(1, 2, 3))
-
-
-  }
-}
