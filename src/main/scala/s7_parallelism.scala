c57af46acddd92e1e507c857b43ca20df8a5a848
eren <eozel44@gmail.com>
2023-12-06 18:31:47 +0300

    initial implementation own par api

--- src/main/scala/s7_parallelism.scala
+++ src/main/scala/s7_parallelism.scala
@@ -1,5 +1,8 @@
 package parallelism
 
+import java.util.concurrent._
+import language.implicitConversions
+
 object s7_parallelism {
 
   /**
@@ -15,6 +18,24 @@ object s7_parallelism {
       val (l,r) = as.splitAt(as.length/2)
       sum(l) + sum(r)
     }
+
+  object Par {
+    type Par[A] = ExecutorService => Future[A]
+
+    /**
+       unit => injects a constant into a parallel computation.
+       map2 => combines the results of two parallel computations with a binary function.
+       fork => spawns a parallel computation. The computation will not be spawned until forced by run.
+       run  => extracts a value from a Par by actually performing the computation.
+     */
+    def run[A](s:ExecutorService)(a:Par[A] ):Future[A] = a(s)
+    def unit[A](a:A):Par[A] = ???
+    def map2[A,B,C](a: Par[A], b: Par[B])(f: (A,B) => C): Par[C] = ???
+    def fork[A](a: => Par[A]): Par[A] = ???
+    def async[A](a: => A): Par[A] = fork(unit(a))
+
+  }
+
   def main(args: Array[String]): Unit = {
 
     val ls = IndexedSeq(1,2,3,4,5,6,7,8,9)
