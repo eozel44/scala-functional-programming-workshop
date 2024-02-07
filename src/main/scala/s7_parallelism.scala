3745b5754510bb4b6fc6a05c86abd5484f68892d
eren <eozel44@gmail.com>
2024-02-07 23:31:01 +0300

    parallelism initial

--- src/main/scala/s7_parallelism.scala
+++ src/main/scala/s7_parallelism.scala
@@ -29,9 +29,24 @@ object s7_parallelism {
        run  => extracts a value from a Par by actually performing the computation.
      */
     def run[A](s:ExecutorService)(a:Par[A] ):Future[A] = a(s)
-    def unit[A](a:A):Par[A] = ???
-    def map2[A,B,C](a: Par[A], b: Par[B])(f: (A,B) => C): Par[C] = ???
-    def fork[A](a: => Par[A]): Par[A] = ???
+
+    def unit[A](a:A):Par[A] = (es: ExecutorService) => UnitFuture(a) // `unit` is represented as a function that returns a `UnitFuture`, which is a simple implementation of `Future` that just wraps a constant value. It doesn't use the `ExecutorService` at all. It's always done and can't be cancelled. Its `get` method simply returns the value that we gave it.
+
+    private case class UnitFuture[A](get: A) extends Future[A] {
+      def isDone = true
+      def get(timeout: Long, units: TimeUnit) = get
+      def isCancelled = false
+      def cancel(evenIfRunning: Boolean): Boolean = false
+    }
+
+    def map2[A,B,C](a: Par[A], b: Par[B])(f: (A,B) => C): Par[C] = (es:ExecutorService) =>{
+      val af = a(es)
+      val bf = b(es)
+      UnitFuture(f(af.get(),bf.get()))
+    }
+    def fork[A](a: => Par[A]): Par[A] = es => es.submit(new Callable[A] {
+      override def call(): A = a(es).get
+    })
     def async[A](a: => A): Par[A] = fork(unit(a))
 
   }
